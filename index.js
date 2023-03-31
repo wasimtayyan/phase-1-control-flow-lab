89034f8265eada22bfb34a36c2409df6bfe73f30
wasim altayyan <wasimtayyan@gmail.com>
2023-03-31 16:31:16 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue<= 400){
+    return 'This one is on me!'
+  }
+  else if (someValue<=2000 ){
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue>2000 && someValue<=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(somewere){
   // Write your code here!
+  
+  return somewere ==='NYC'? 'Ok, sounds good.': 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+        return 'Thank you so much.';
+    case  'not as generous':
+      return 'Thank you.' 
+    default:
+      return  'Bye.'
+  }
+   
 }
+console.log(scuberGreetingForFeet(2300)) 
+console.log(ternaryCheckCity('NYC')) 
